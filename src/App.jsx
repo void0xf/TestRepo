c08002fd1e64e2e27c4cb555f2c5ece6f81383f4
void0xf <lucyfer99100@gmail.com>
2024-07-26 19:35:06 +0200

    feat: implement handle count3

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={hanldeCount2}>count is {count}</button>
+        <button onClick={hanldeCount3}>count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
